52b29039e6072fda4ae1f5364078f784d764235b
Cristóbal <cristobal.becerraf@usach.cl>
2019-05-21 01:44:03 -0400

    new tracks

--- twitter-to-pubsub/twitter-to-pubsub.py
+++ twitter-to-pubsub/twitter-to-pubsub.py
@@ -8,8 +8,8 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
+# Publicar tweets en el topic deseado
 def publish(client, pubsub_topic, data_lines):
-    """Publish to the given pubsub topic."""
     messages = []
     for line in data_lines:
         pub = base64.urlsafe_b64encode(line)
@@ -19,28 +19,24 @@ def publish(client, pubsub_topic, data_lines):
         topic=pubsub_topic, body=body).execute(num_retries=3)
     return resp
 
+# Listener de tweets
 class StdOutListener(StreamListener):
-    """A listener handles tweets that are received from the stream.
-    This listener dumps the tweets into a PubSub topic
-    """
     count = 0
     twstring = ''
     tweets = []
-    batch_size = 50
-    total_tweets = 10000000
+    batch_size = 50 #cuantos tweets extraer por request
+    total_tweets = 10000000 #detener al llegar a total_tweets
     client = utils.create_pubsub_client(utils.get_credentials())
 
     def write_to_pubsub(self, tw):
         publish(self.client, 'projects/sd-3-241301/topics/twitter', tw)
 
     def on_data(self, data):
-        """What to do when tweet data is received."""
         self.tweets.append(data)
         if len(self.tweets) >= self.batch_size:
             self.write_to_pubsub(self.tweets)
             self.tweets = []
         self.count += 1
-        # if we've grabbed more than total_tweets tweets, exit the script.
         if self.count > self.total_tweets:
             return False
         if (self.count % 1000) == 0:
@@ -55,4 +51,4 @@ if __name__ == '__main__':
     auth = OAuthHandler('D1kq9ASSkLHlCAWQwQoa0ncph', 'wVyihyOMjlXzIs9aqSVbHGtgpwrhXuzMqT2EZoHOIQ9mluc6Yk')
     auth.set_access_token('1016414386162995200-je8a5PynhzO9NLu9hA1bDzbk7UJ7Qg', 'gsHfXyN4DJiqgARC7zZQVZzSKpFF9FNcndgNy94OQAjLW')
     stream = Stream(auth, listener)
-    stream.filter(languages=['en'], track=['buzzwords', 'pubsub', 'appengine', 'dataflow', 'bigquery', 'google', 'gcp'])
+    stream.filter(languages=['es'], track=['chile','usach','google','cloud'])
